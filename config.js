7b82490f7c6726972d77553de3949db2b5152991
Ritvik <114260124+ritvikneu@users.noreply.github.com>
2023-10-11 21:53:44 -0400

    Assign4 (#2)
    * optimized
    
    * optimized

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    numOfPubSubnets: 3,
+    numOfPriSubnets: 3,
+    baseCIDRBlock: "10.0.0.0/16",
+    subnetCIDR: "10.0.0.0",
+    availabilityZone: "us-east-1",
+    vpcName: "myVpcDev",
+    igName: "igw"
+};
