217cae36c3211814aa2895d609a8f3cf11cc3b75
Ritvik <114260124+ritvikneu@users.noreply.github.com>
2023-11-24 18:50:26 -0500

    gcp storage (#31)

--- Archive/index.js
+++ Archive/index.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const { Storage } = require('@google-cloud/storage');
+const fs = require('fs');
 const mailgun = require('mailgun-js')({
     apiKey: "e8500bfb35a6d00230540dada439281e-5d2b1caa-884ab131",
     domain: "demo.ritvikparamkusham.me",
@@ -7,7 +9,7 @@ const mailgun = require('mailgun-js')({
  
 exports.handler = async (event, context) => {
   try {
-    const fetch = await import('node-fetch'); // Dynamic import for node-fetch
+    const fetch = await require('node-fetch'); // Dynamic import for node-fetch
  
     console.log(event);
     const snsMessage = JSON.parse(event.Records[0].Sns.Message);
@@ -16,29 +18,28 @@ exports.handler = async (event, context) => {
     console.log(fileUrl);
 
 
-     // Download the file using node-fetch
-    const response = await fetch(fileUrl);
-    const fileBuffer = await response.buffer(); // Get the file content as a buffer
-
-    // Set up Google Cloud Storage
-    const storage = new Storage({
-      projectId: 'prime-cosmos-405923', // Replace with your GCP project ID
-      keyFilename: '/Users/ritvikparamkusham/Downloads/fileName.json', // Replace with the path to your GCP service account key file
-    });
-
-    const bucketName = 'my-bucket-check-751d6cb'; // Replace with your GCS bucket name
-    const fileName = '/Users/ritvikparamkusham/Downloads/fileName.json'; // Replace with the desired file name in GCS
-
-    // Upload the file to GCS bucket
-    const bucket = storage.bucket(bucketName);
-    const file = bucket.file(fileName);
-    await file.save(fileBuffer);
-
-    console.log('File uploaded to GCS successfully.');
-
-
-
-
+      // Download the file using node-fetch
+      const response = await fetch(fileUrl);
+      const fileBuffer = await response.buffer(); // Get the file content as a buffer
+  
+      const keyFileJson = JSON.parse(fs.readFileSync('prime-cosmos-405923-cd296041e562.json'));
+      
+      const storage = new Storage({
+          projectId: 'prime-cosmos-405923',
+          credentials: keyFileJson, // Pass the parsed JSON content as credentials
+      });
+  
+      // const bucketName = 'my-bucket-check-751d6cb'; // Replace with your GCS bucket name
+      const fileName = 'submision.zip'; // Replace with the desired file name in GCS
+  
+      // Upload the file to GCS bucket
+      const bucket = storage.bucket(process.env.GOOGLE_STORAGE_BUCKET_NAME);
+      const file = bucket.file(fileName);
+      await file.save(fileBuffer);
+  
+      console.log('File uploaded to GCS successfully.');
+  
+      
     const emailData = {
         from: "noreply@demo.ritvikparamkusham.me",
         to: "ritvik.param@gmail.com",
